5aa28e24d259495c1225424be7acddd4eb0cc8a8
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-21 16:05:00 +0400

    set up tailwindcss

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-3xl font-bold text-red-500 underline">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
       </h1>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
