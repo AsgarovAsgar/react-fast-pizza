e2d47fca29c9a9a33e96763059c2798def143a98
Asgar Asgarov <asgarovasgar28@gmail.com>
2025-04-21 12:05:17 +0400

    fetch orders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,58 +1,15 @@
 // Test ID: IIDSAT
 
-import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
+import { useLoaderData } from 'react-router-dom'
+import { getOrder } from '../../services/apiRestaurant'
+import { calcMinutesLeft, formatCurrency, formatDate } from '../../utils/helpers'
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
+export default function Order() {
+  const order = useLoaderData()
 
-function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
-  const deliveryIn = calcMinutesLeft(estimatedDelivery);
+  const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } = order
+  const deliveryIn = calcMinutesLeft(estimatedDelivery)
 
   return (
     <div>
@@ -67,9 +24,7 @@ function Order() {
 
       <div>
         <p>
-          {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+          {deliveryIn >= 0 ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃` : 'Order should have arrived'}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
@@ -80,7 +35,7 @@ function Order() {
         <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default Order;
+export const loader = async ({ params }) => await getOrder(params.orderId)
